e1ec1c16e8ea7ef275b9736d2dfcd8ed7f666ea2
uto8 <70425214+uto8@users.noreply.github.com>
2022-11-19 12:37:34 +0900

    実車率修正

--- app/controllers/actual_vehicle_rates_controller.rb
+++ app/controllers/actual_vehicle_rates_controller.rb
@@ -9,18 +9,17 @@ class ActualVehicleRatesController < ApplicationController
     @q = DeliveryContent.ransack(params[:q])
     @delivery_contents_results = @q.result(distinct: true)
 
+
     array = []
     @delivery_contents_results.each do |data|
-      array.push(data.delivery_roots.sum(:distance))
+      array.push(data.distance)
     end
     @total_distance = array.sum
 
     array2 = []
     @delivery_contents_results.each do |data|
-      data.delivery_roots.each do |root|
-        if root.is_loaded?
-          array2.push(root.distance)
-        end
+      if data.loading_weight != 0
+        array2.push(data.distance)
       end
     end
     @actual_vehicle_distance = array2.sum
