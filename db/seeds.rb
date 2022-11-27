da191c37b75121a512e51414456b400e4a56635d
uto8 <70425214+uto8@users.noreply.github.com>
2022-11-27 16:39:12 +0900

    deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,44 +36,42 @@ User.create(
 end
 
 # トラック作成
-# 5.times do |n|
-#   Truck.create!(
-#     truck_number: 1234 + n,
-#     load_capacity: 2000 + n * 1000,
-#     car_size: n + 5,
-#     carrier: 1,
-#     car_weight: n * 1000
-#   )
-# end
+5.times do |n|
+  Truck.create!(
+    truck_number: 1234 + n,
+    load_capacity: 2000 + n * 1000,
+    carrier: 1
+  )
+end
 
 # #納入先情報
-# 5.times do |n|
-#   DeliveryDestination.create!(
-#     delivery_destination_name: "納入先#{n}",
-#     delivery_destination_address: "愛知県名古屋市瑞穂区",
-#     commercial_distribution: "商流#{n}"
-#   )
-# end
+5.times do |n|
+  DeliveryDestination.create!(
+    delivery_destination_name: "納入先#{n}",
+    delivery_destination_address: "愛知県名古屋市瑞穂区",
+    commercial_distribution: "商流#{n}"
+  )
+end
 
-# # 配送データ作成
-# 5.times do |n|
-#   DeliveryContent.create!(
-#     user: User.find_by(email: "driver#{n}@gmail.com"),
-#     truck: Truck.find_by(truck_number: 1234 + n),
-#     delivery_date: 20220826,
-#     consignor: "豊田通商#{n}",
-#     delivery_destination: DeliveryDestination.find_by(delivery_destination_name: "納入先#{n}"),
-#     distance: 2000 + n * 1000,
-#     loading_weight: 100 + n * 100
-#   )
-# end
+# 配送データ作成
+5.times do |n|
+  DeliveryContent.create!(
+    user: User.find_by(email: "driver#{n}@gmail.com"),
+    truck: Truck.find_by(truck_number: 1234 + n),
+    delivery_date: 20220826,
+    consignor: "豊田通商#{n}",
+    delivery_destination: DeliveryDestination.find_by(delivery_destination_name: "納入先#{n}"),
+    distance: 2000 + n * 1000,
+    loading_weight: 100 + n * 100
+  )
+end
 
 
-# # 荷物データ作成
-# 5.times do |n|
-#   Load.create!(
-#     load_number: 1111 + n,
-#     material: "鉄",
-#     delivery_content: DeliveryContent.find_by(consignor: "豊田通商#{n}")
-#   )
-# end
+# 荷物データ作成
+5.times do |n|
+  Load.create!(
+    load_number: 1111 + n,
+    material: "鉄",
+    delivery_content: DeliveryContent.find_by(consignor: "豊田通商#{n}")
+  )
+end
